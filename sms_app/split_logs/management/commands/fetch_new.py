b9c857aad545fa6c60d6e084bc7a4018f726095c
Oleg Demakov <oleg.demakov@epfl.ch>
2019-04-15 11:37:48 +0200

    log command

--- sms_app/split_logs/management/commands/fetch_new.py
+++ sms_app/split_logs/management/commands/fetch_new.py
@@ -1,19 +1,24 @@
 import datetime
 import subprocess
+import logging
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Fetch new tracking logs files'
 
     def handle(self, *args, **options):
         if settings.TRACKING_LOGS_ORIGINAL_SRC and settings.TRACKING_LOGS_ORIGINAL_DST:
-            subprocess.call([
+            cmd = [
                 "rsync",
                 "-av",
                 "--include='*.gz'",
                 "--exclude='*'",
                 settings.TRACKING_LOGS_ORIGINAL_SRC,
                 settings.TRACKING_LOGS_ORIGINAL_DST
-            ])
+            ]
+            logger.info("run command: ".format(" ".join(cmd)))
+            subprocess.call()
