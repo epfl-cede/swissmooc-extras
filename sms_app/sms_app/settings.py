fc18d0c15d9f95087f0a7614d56f516cc492a444
Oleg Demakov <oleg.demakov@epfl.ch>
2019-04-15 11:27:16 +0200

    added split_logs add

--- sms_app/sms_app/settings.py
+++ sms_app/sms_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'split_logs.apps.SplitLogsConfig',
     'check_ssl.apps.CheckSslConfig',
     'django.contrib.admin',
     'django.contrib.auth',
