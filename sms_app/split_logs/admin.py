ac0e7798cff0b4315d076787192bdfa50ae4e822
Oleg Demakov <oleg.demakov@epfl.ch>
2019-04-18 12:52:55 +0200

    list_filter for processed files

--- sms_app/split_logs/admin.py
+++ sms_app/split_logs/admin.py
@@ -6,6 +6,7 @@ class DirOriginalAdmin(admin.ModelAdmin):
     list_display = ('name', 'created', 'updated')
 class FileOriginalAdmin(admin.ModelAdmin):
     list_display = ('fullname', 'lines_total', 'lines_error', 'created', 'updated')
+    list_filter = ['dir_original']
 
 admin.site.register(DirOriginal, DirOriginalAdmin)
 admin.site.register(FileOriginal, FileOriginalAdmin)
