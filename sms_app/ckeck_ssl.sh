30d88fc74777892e3be8f48e71ec29c0d5e3d721
Oleg Demakov <oleg.demakov@epfl.ch>
2019-05-14 14:20:36 +0200

    do not show sourced ENV variables

--- sms_app/ckeck_ssl.sh
+++ sms_app/ckeck_ssl.sh
@@ -2,7 +2,7 @@
 set -ex
 
 logger run /home/ubuntu/sms-exrtas/sms_app.sh
-source /home/ubuntu/sms_app.env
+source /home/ubuntu/sms_app.env >> /dev/null 2>&1
 source /home/ubuntu/sms-exrtas/venv/bin/activate
 
 cd /home/ubuntu/sms-exrtas/sms_app
