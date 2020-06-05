708cd580d5bb6238ee72975f0b8b20e38424d528
Oleg Demakov <oleg.demakov@epfl.ch>
2020-06-05 08:30:59 +0200

    send email if SSL expites soon

--- sms_app/check_ssl/management/commands/check_ssl.py
+++ sms_app/check_ssl/management/commands/check_ssl.py
@@ -4,6 +4,7 @@ import ssl
 import socket
 
 
+from django.core.mail import send_mail
 from django.core.management.base import BaseCommand, CommandError
 
 from check_ssl.models import Site
@@ -17,26 +18,61 @@ class Command(BaseCommand):
         sites = Site.objects.all()
         now = datetime.datetime.now()
         result = 0
+        mail_log = {
+            'for_update': [],
+            'with_error': [],
+        }
         for site in sites:
             try:
                 expires = self._ssl_expiry_datetime(site.hostname)
             except ssl.CertificateError as e:
                 site.error = 'cert error {}'.format(e)
+                mail_log['with_error'].append("{}\t{}".format(site.hostname, site.error))
             except ssl.SSLError as e:
                 site.error = 'cert error {}'.format(e)
+                mail_log['with_error'].append("{}\t{}".format(site.hostname, site.error))
             except socket.timeout as e:
                 site.error = 'could not connect'
+                mail_log['with_error'].append("{}\t{}".format(site.hostname, site.error))
             except socket.gaierror as e:
                 site.error = 'not accessable'
+                mail_log['with_error'].append("{}\t{}".format(site.hostname, site.error))
             else:
                 site.expires = expires
                 site.error = ''
                 if expires - now < WARNING_DELTA:
                     self.stdout.write(self.style.ERROR('Site SSL cert will expire at %s, have to update cert' % expires))
                     result = 1
+                    mail_log['for_update'].append("{}\t{}".format(site.hostname, site.expires))
+
             site.save()
 
+        self._send_mail(mail_log)
         sys.exit(result)
+
+    def _send_mail(mail_log):
+        now = datetime.datetime.now().date()
+        send_mail(
+            '[SMS-extras:{env}] Check SSL result - {date}'.format(
+                env=settings.SMS_APP_ENV,
+                date=now
+            ),
+            '''
+Need your attention!
+
+Expired soon hostnames:
+{}
+With errors hostnames:
+{}
+            '''.format(
+                now,
+                '\n'.join(mail_log['for_update']),
+                '\n'.join(mail_log['with_error']),
+            ),
+            settings.EMAIL_FROM_ADDRESS,
+            settings.EMAIL_TO_ADDRESSES,
+            fail_silently=False,
+        )
 
     def _ssl_expiry_datetime(self, hostname: str) -> datetime.datetime:
         ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
