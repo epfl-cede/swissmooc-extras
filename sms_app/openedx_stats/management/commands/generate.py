94ebf831f698dab0764b85d536a2a34a49d915d5
Oleg Demakov <oleg.demakov@epfl.ch>
2022-03-11 15:49:30 +0100

    log processed database

--- sms_app/openedx_stats/management/commands/generate.py
+++ sms_app/openedx_stats/management/commands/generate.py
@@ -15,7 +15,6 @@ class Command(BaseCommand):
     help = 'Generate basic stats from DB'
 
     database = ''
-    cursor = None
     course_id = None
 
     def handle(self, *args, **options):
@@ -23,18 +22,19 @@ class Command(BaseCommand):
             # skip default, id and old databases
             if self.database in ['default', 'edxapp_id', 'edxapp_readonly']: continue
             with connections[self.database].cursor() as cursor:
+                print(' ===> process {}'.format(self.database))
                 # get course_id
-                for course_id in self.course_ids(cursor):
-                    self.student_enrolments_by_date(cursor, course_id)
-                    self.student_enrolments_by_mode(cursor, course_id)
+                for self.course_id in self.get_course_ids(cursor):
+                    self.student_enrolments_by_date(cursor)
+                    self.student_enrolments_by_mode(cursor)
 
-    def student_enrolments_by_date(self, cursor, course_id):
-        enrollments = self.enrolments_by_date(cursor, course_id)
+    def student_enrolments_by_date(self, cursor):
+        enrollments = self.get_enrolments_by_date(cursor)
         filename = '{}/{}/{}/{}.csv'.format(
             settings.STATS_FILE_PATH,
             self.database.split('_')[1],
             'student_enrolments_by_date',
-            course_id
+            self.course_id
         )
         pathlib.Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
         with open(filename, 'w', newline='') as csvfile:
@@ -42,13 +42,13 @@ class Command(BaseCommand):
             for r in enrollments:
                 writer.writerow(r)
 
-    def student_enrolments_by_mode(self, cursor, course_id):
-        enrollments = self.enrolments_by_mode(cursor, course_id)
+    def student_enrolments_by_mode(self, cursor):
+        enrollments = self.get_enrolments_by_mode(cursor)
         filename = '{}/{}/{}/{}.csv'.format(
             settings.STATS_FILE_PATH,
             self.database.split('_')[1],
             'student_enrolments_by_mode',
-            course_id
+            self.course_id
         )
         pathlib.Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
         with open(filename, 'w', newline='') as csvfile:
@@ -56,14 +56,14 @@ class Command(BaseCommand):
             for r in enrollments:
                 writer.writerow(r)
 
-    def course_ids(self, cursor):
+    def get_course_ids(self, cursor):
         cursor.execute("select DISTINCT course_id from student_courseenrollment")
         return [row[0] for row in cursor.fetchall()]
 
-    def enrolments_by_date(self, cursor, course_id):
-        cursor.execute("select date(created),count(*) from student_courseenrollment where course_id=%s GROUP BY 1", [course_id])
+    def get_enrolments_by_date(self, cursor):
+        cursor.execute("select date(created),count(*) from student_courseenrollment where course_id=%s GROUP BY 1", [self.course_id])
         return cursor.fetchall()
 
-    def enrolments_by_mode(self, cursor, course_id):
-        cursor.execute("select mode,count(*) from student_courseenrollment where course_id=%s GROUP BY 1", [course_id])
+    def get_enrolments_by_mode(self, cursor):
+        cursor.execute("select mode,count(*) from student_courseenrollment where course_id=%s GROUP BY 1", [self.course_id])
         return cursor.fetchall()
