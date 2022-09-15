a4ae0f451a43581a6fc341d0deb9b8dd02ea8bc0
Oleg Demakov <oleg.demakov@epfl.ch>
2022-09-15 11:27:27 +0200

    add sms_app/course_xml_dump.sh script

---
+++ sms_app/course_xml_dump.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+logger run /home/ubuntu/sms-extras/sms_app.sh
+source /home/ubuntu/sms_app.env >> /dev/null 2>&1
+source /home/ubuntu/sms-extras/venv/bin/activate
+
+cd /home/ubuntu/sms-extras/sms_app
+
+python manage.py course_xml_dump
