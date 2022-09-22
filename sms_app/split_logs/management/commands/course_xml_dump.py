0405a219078fa99d726da922a4a63950fb02162a
Oleg Demakov <oleg.demakov@epfl.ch>
2022-09-22 20:09:58 +0200

    update course_xml_dump command

--- sms_app/split_logs/management/commands/course_xml_dump.py
+++ sms_app/split_logs/management/commands/course_xml_dump.py
@@ -10,39 +10,35 @@ import gnupg
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from split_logs.models import Organisation
+from split_logs.sms_command import SMSCommand
 from split_logs.utils import bucket_name
 from split_logs.utils import dump_course
-from split_logs.utils import handle_verbosity
 from split_logs.utils import run_command
-from split_logs.utils import sms_app_send_email
 from split_logs.utils import SplitLogsUtilsDumpCourseException
 from split_logs.utils import SplitLogsUtilsUploadFileException
 from split_logs.utils import upload_file
 
 
-logger = logging.getLogger(__name__)
-
 class CourseXmlDumpException(Exception):
     """Base class for other exceptions"""
     pass
 
-class Command(BaseCommand):
-    help = 'Course XML dump; running on one of the swarm server during the night'
+class Command(SMSCommand):
+    help = "Course XML dump; running on one of the swarm server during the night"
 
-    message = []
-    now = datetime.datetime.now().date()
+    logger = logging.getLogger(__name__)
 
     def handle(self, *args, **options):
-        handle_verbosity(options)
+        self.handle_verbosity(options)
 
         course_data_for_email_ok = defaultdict(list)
         course_data_for_email_ko = defaultdict(list)
 
         organisations = Organisation.objects.filter(active=True)
         for org in organisations:
-            logger.info("process organisation %s", org.name)
+            self.info(f"organisation: {org.name}")
 
-            self.import_dir = '/var/lib/docker/volumes/openedx-{}_openedx-data/_data/course_export'.format(
+            self.import_dir = "/var/lib/docker/volumes/openedx-{}_openedx-data/_data/course_export".format(
                 org.name.lower()
             )
 
@@ -51,20 +47,20 @@ class Command(BaseCommand):
             self._create_org_dir(org_destination_dir)
 
             for course_id in self._get_courses(org):
-                logger.info('process course_id %s', course_id)
+                self.info(f"course: {course_id}")
                 try:
                     course_file = dump_course(org, course_id, org_destination_dir)
                     course_file_encrypted = self._encrypt(org, course_file)
                     self._upload(org, course_file_encrypted)
                     course_data_for_email_ok[org.name] += (course_id,)
                 except SplitLogsUtilsDumpCourseException as error:
-                    logger.error("SplitLogsUtilsDumpCourseException: %s" % error)
+                    self.error(f"dump course: <{error}>")
                     course_data_for_email_ko[org.name] += (course_id,)
                 except SplitLogsUtilsUploadFileException as error:
-                    logger.error("SplitLogsUtilsUploadFileException: %s" % error)
+                    self.error(f"upload: <{error}>")
                     course_data_for_email_ko[org.name] += (course_id,)
                 except CourseXmlDumpException as error:
-                    logger.error("CourseXmlDumpException: %s" % error)
+                    self.error(f"encrypt: <{error}>")
                     course_data_for_email_ko[org.name] += (course_id,)
 
         self._send_email(course_data_for_email_ok, course_data_for_email_ko)
@@ -74,20 +70,20 @@ class Command(BaseCommand):
             bucket_name(org),
             org,
             fname,
-            '{org}/dump-xml/{date}/{name}'.format(
+            "{org}/dump-xml/{date}/{name}".format(
                 org=org.name,
-                date=fname.split('/')[-2],
+                date=fname.split("/")[-2],
                 name=os.path.basename(fname)
             )
         )
 
     def _encrypt(self, org, fname):
         gpg = gnupg.GPG()
-        gpg.encoding = 'utf-8'
+        gpg.encoding = "utf-8"
         importres = gpg.import_keys(org.public_key.value)
-        gpg.trust_keys(importres.fingerprints, 'TRUST_ULTIMATE')
-        fname_encrypted = '{}.gpg'.format(fname)
-        with open(fname, 'rb') as f:
+        gpg.trust_keys(importres.fingerprints, "TRUST_ULTIMATE")
+        fname_encrypted = "{}.gpg".format(fname)
+        with open(fname, "rb") as f:
             status = gpg.encrypt_file(
                 f,
                 armor=True,
@@ -97,11 +93,11 @@ class Command(BaseCommand):
             if status.ok:
                 os.remove(fname)
             else:
-                raise CourseXmlDumpException('Encrypt file error')
+                raise CourseXmlDumpException("Encrypt file error")
         return fname_encrypted
 
     def _organisation_dir(self, organisation):
-        return '{}/{}/{}/'.format(
+        return "{}/{}/{}/".format(
             settings.DUMP_XML_PATH,
             organisation.name.lower(),
             self.now
@@ -119,28 +115,30 @@ class Command(BaseCommand):
     def _send_email(self, ok, ko):
         nok = sum([len(v) for v in ok.values()])
         nko = sum([len(v) for v in ko.values()])
-        body = 'Course XML dump results - {}:\n\nDumped {} courses out of {}'.format(
-            self.now,
-            nok,
-            nok + nko
-        )
+        result_message = []
+        result_message.append(f"{nok} courses were dumped out of {nok + nko}")
         if nko > 0:
-            body += '\n\nERRORS IN THE COURSES:\n{}'.format(
-                '\n'.join(['\n' + k + ':\n' + '\n'.join(v) for k,v in ko.items()])
-            )
-        sms_app_send_email(
-            f"Course XML dump result - {self.now}",
-            body,
-        )
+            result_message.append("There are errors in the courses below:")
+            for org, courses in ko.items():
+                result_message.append("{org}")
+                for course in courses:
+                    result_message.append("{course}")
+
+        result_message.append("")
+        result_message.append("Detailed log:")
+
+        self.message = result_message + self.message
+
+        self.send_email(f"Course XML dump")
 
     def _get_courses(self, org):
         return_code, stdout, stderr = run_command([
-            'ssh', 'ubuntu@zh-%s-swarm-1' % settings.SMS_APP_ENV,
-            '/home/ubuntu/.local/bin/docker-run-command', 'openedx-%s_cms' % org.name.lower(),
-            'python', 'manage.py', 'cms', '--settings=tutor.production', 'dump_course_ids'
+            "ssh", "ubuntu@zh-%s-swarm-1" % settings.SMS_APP_ENV,
+            "/home/ubuntu/.local/bin/docker-run-command", "openedx-%s_cms" % org.name.lower(),
+            "python", "manage.py", "cms", "--settings=tutor.production", "dump_course_ids"
         ])
         if return_code != 0:
-            logger.error('get course list error: %s', stderr)
+            self.error(f"get course list error: <{stderr}>")
             return []
 
-        return stdout.decode().strip('\n').split('\n')[1:]
+        return stdout.strip("\n").split("\n")[1:]
