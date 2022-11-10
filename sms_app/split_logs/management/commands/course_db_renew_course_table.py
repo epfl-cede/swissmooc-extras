86ec7b2ac6ca0bcab4b5a35641fd16ae121ad609
Oleg Demakov <oleg.demakov@epfl.ch>
2022-11-10 15:16:27 +0100

    course_db_renew_course_table get courses from docker instances

--- sms_app/split_logs/management/commands/course_db_renew_course_table.py
+++ sms_app/split_logs/management/commands/course_db_renew_course_table.py
@@ -17,20 +17,23 @@ class Command(SMSCommand):
     def handle(self, *args, **options):
         self.handle_verbosity(options)
 
-        organisations = Organisation.objects.filter(active=True)
-        for o in organisations:
-            self.info(f"process organization {o.name}")
-            Course.objects.filter(organisation=o).update(active=NOT_ACTIVE)
+        for organisation in Organisation.objects.filter(active=True):
+            self.info(f"process organization {organisation.name}")
+            Course.objects.filter(organisation=organisation).update(active=NOT_ACTIVE)
 
             cursor = self.edxapp_cursor()
-            cursor.execute("SELECT course_id FROM student_courseenrollment GROUP BY course_id")
+            cursor.execute(
+                "SELECT course_id FROM {db_name}.student_courseenrollment GROUP BY course_id".format(
+                    db_name="docker_" + organisation.name.lower() + "_edxapp",
+                )
+            )
             for row in cursor.fetchall():
                 name = row[0]
                 try:
                     self.info(f"Set course {name} as active")
-                    course = Course.objects.get(name=name, organisation=o)
+                    course = Course.objects.get(name=name, organisation=organisation)
                     course.active = ACTIVE
                     course.save()
                 except ObjectDoesNotExist:
                     self.info(f"Insert new course <{name}>")
-                    Course.objects.create(name=row[0], organisation=o, active=ACTIVE)
+                    Course.objects.create(name=row[0], organisation=organisation, active=ACTIVE)
