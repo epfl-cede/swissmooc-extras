8707d2c5bb3b1a5daac17b8782a616ff8a2fc8da
Oleg Demakov <oleg.demakov@epfl.ch>
2022-11-11 09:35:12 +0100

    fix env file path

--- sms_app/course_xml_dump.sh
+++ sms_app/course_xml_dump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 logger run /data/sms-extras/sms_app.sh
-source /data/sms_app.env >> /dev/null 2>&1
+source /data/sms-extras.env >> /dev/null 2>&1
 source /data/sms-extras/venv/bin/activate
 
 cd /data/sms-extras/sms_app
