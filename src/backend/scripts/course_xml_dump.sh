319c17812cc43a615be3e884b31d58895a0c05ff
Oleg Demakov <oleg.demakov@epfl.ch>
2023-11-21 12:02:42 +0100

    command course_xml_dump runs each organization separatly

--- src/backend/scripts/course_xml_dump.sh
+++ src/backend/scripts/course_xml_dump.sh
@@ -4,4 +4,12 @@ logger run /data/swissmooc-extras/src/backend/scripts/course_xml_dump.sh
 
 source /data/swissmooc-extras/venv/bin/activate
 python /data/swissmooc-extras/src/backend/manage.py course_db_renew_course_table -v 2
-python /data/swissmooc-extras/src/backend/manage.py course_xml_dump -v 2
+python /data/swissmooc-extras/src/backend/manage.py course_xml_dump --org epfl -v 2
+python /data/swissmooc-extras/src/backend/manage.py course_xml_dump --org ethz -v 2
+python /data/swissmooc-extras/src/backend/manage.py course_xml_dump --org ffhs -v 2
+python /data/swissmooc-extras/src/backend/manage.py course_xml_dump --org implicit_bias -v 2
+python /data/swissmooc-extras/src/backend/manage.py course_xml_dump --org sms -v 2
+python /data/swissmooc-extras/src/backend/manage.py course_xml_dump --org unige -v 2
+python /data/swissmooc-extras/src/backend/manage.py course_xml_dump --org unili -v 2
+python /data/swissmooc-extras/src/backend/manage.py course_xml_dump --org usi -v 2
+python /data/swissmooc-extras/src/backend/manage.py course_xml_dump --org zhaw -v 2
